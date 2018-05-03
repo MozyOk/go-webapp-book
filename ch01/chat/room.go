60a8c2ac8ec47c1a1d478d646bb456ab43449faa
MozyOk <mozy.okubo@gmail.com>
2018-05-04 02:08:46 +0900

    feat: use select statement  for parallel processing

--- ch01/chat/room.go
+++ ch01/chat/room.go
@@ -9,3 +9,25 @@ type room struct {
 
 	clients map[*client]bool
 }
+
+func (r *room) run() {
+	for {
+		select {
+		case client := <-r.join:
+			r.clients[client] = true
+		case client := <-r.leave:
+			delete(r.clients, client)
+			close(client.send)
+		case msg := <-r.forward:
+			for client := range r.clients {
+				select {
+				case client.send <- msg:
+
+				default:
+					delete(r.clients, client)
+					close(client.send)
+				}
+			}
+		}
+	}
+}
