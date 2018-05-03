5e152ff0f7b4e994e2b2059c6d4b5f18245a4ef6
MozyOk <mozy.okubo@gmail.com>
2018-05-04 01:51:34 +0900

    feat: add WebSocket read & write method

--- ch01/chat/client.go
+++ ch01/chat/client.go
@@ -1,4 +1,5 @@
 package main
+
 import (
 	"github.com/gorilla/websocket"
 )
@@ -9,4 +10,24 @@ type client struct {
 	send chan []byte
 
 	room *room
+}
+
+func (c *client) read() {
+	for {
+		if _, msg, err := c.socket.ReadMessage(); err == nil {
+			c.room.forward <- msg
+		} else {
+			break
+		}
+	}
+	c.socket.Close()
+}
+
+func (c *client) write() {
+	for msg := range c.send {
+		if err := c.socket.WriteMessage(websocket.TextMessage, msg); err != nil {
+			break
+		}
+	}
+	c.socket.Close()
 }
